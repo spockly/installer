a4aff6f767baf84fe6a7092d3aa036f4503f4d85
spockly <spockly@gmail.com>
2020-08-16 22:29:04 -0400

    Update training_install.sh

--- training_install.sh
+++ training_install.sh
@@ -28,9 +28,12 @@ sudo ./msfinstall
 echo "Metasploit is installed"
 msfupdate
 echo "Installing Hashcat"
-sudo apt-get install -y ocl-icd-libopencl1  nvidia-cuda-toolkit
-sudo apt-get install mesa-common-dev
-sudo apt-get install freeglut3-dev -y
+wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2004/x86_64/cuda-ubuntu2004.pinsudo 
+mv cuda-ubuntu2004.pin /etc/apt/preferences.d/cuda-repository-pin-600
+wget https://developer.download.nvidia.com/compute/cuda/11.0.3/local_installers/cuda-repo-ubuntu2004-11-0-local_11.0.3-450.51.06-1_amd64.deb
+sudo dpkg -i cuda-repo-ubuntu2004-11-0-local_11.0.3-450.51.06-1_amd64.deb
+sudo apt-key add /var/cuda-repo-ubuntu2004-11-0-local/7fa2af80.pub
+sudo apt-get updatesudo apt-get -y install cuda
 cd installers
 wget https://hashcat.net/files/hashcat-6.1.1.7z
 sudo p7zip -d hashcat-6.1.1.7z
